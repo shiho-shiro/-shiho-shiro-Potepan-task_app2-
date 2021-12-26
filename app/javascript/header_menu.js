fdb7dbcff739fab316ad0df8b3937d900a0afcf6
Shiho <seinoshiho@jouyashihonoMacBook-Air.local>
2021-12-26 21:35:21 +0900

    header,footer完成

--- app/javascript/header_menu.js
+++ app/javascript/header_menu.js
@@ -1,14 +1,15 @@
 $(function() {
-	$("#acount").click(function() {
-		if ($('#menus').is(':hidden')) {
-			// 非表示の場合の処理
-			$("#menus").css("visibility", "hidden");
-		} else {
-			// 表示されている場合の処理
-			$("#menus").css("visibility", "visible");
-		}
-	});
+	$(document).on('click', '#acount', function(){
+		// 表示に設定
+		$("#menus").toggle();
+	  });
+	  
 });
+
+
+
+
+
 
 
 
